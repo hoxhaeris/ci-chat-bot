971b79893e5d0916bb8e2c49a23d1301a78a5486
Eris Hoxha <ehoxha@redhat.com>
2023-01-23 14:42:59 +0100

    checkpoint

--- pkg/slack/modals/launch/launch.go
+++ pkg/slack/modals/launch/launch.go
@@ -15,66 +15,64 @@ const Identifier modals.Identifier = "launch"
 const Identifier2ndStep modals.Identifier = "launch2ndStep"
 const Identifier3rdStep modals.Identifier = "launch3ddStep"
 
-func launchNext(updater modals.ViewUpdater, jobmanager manager.JobManager) interactions.Handler {
-	return interactions.HandlerFunc("launch2", func(callback *slack.InteractionCallback, logger *logrus.Entry) (output []byte, err error) {
-		response, err := json.Marshal(&slack.ViewSubmissionResponse{
-			ResponseAction: slack.RAUpdate,
-			View:           SecondStepView(callback, nil, jobmanager),
-		})
-		if err != nil {
-			logger.WithError(err).Error("Failed to marshal FirstStepView update submission response.")
-			return nil, err
-		}
-		return response, nil
-	})
-}
-
-func launchNext2(updater modals.ViewUpdater, jobmanager manager.JobManager) interactions.Handler {
-	return interactions.HandlerFunc("launch2", func(callback *slack.InteractionCallback, logger *logrus.Entry) (output []byte, err error) {
-		response, err := json.Marshal(&slack.ViewSubmissionResponse{
-			ResponseAction: slack.RAUpdate,
-			View:           ThirdStepView(callback, nil, jobmanager),
-		})
-		if err != nil {
-			logger.WithError(err).Error("Failed to marshal FirstStepView update submission response.")
-			return nil, err
-		}
-		return response, nil
-	})
-}
-
-// processSubmissionHandler files a Jira issue for this form
-func processSubmissionHandler(updater modals.ViewUpdater, jobmanager manager.JobManager) interactions.Handler {
-	return launchNext(updater, jobmanager)
-}
-
-// processSubmissionHandler files a Jira issue for this form
-func processSubmissionHandler2ndStep(updater modals.ViewUpdater, jobmanager manager.JobManager) interactions.Handler {
-	return launchNext2(updater, jobmanager)
-}
-
-// processSubmissionHandler files a Jira issue for this form
-func processSubmissionHandler3rdStep(updater modals.ViewUpdater, jobmanager manager.JobManager) interactions.Handler {
-	return launchNext(updater, jobmanager)
-}
-
-// RegisterFirstStep creates a registration entry for the enhancement request form
 func RegisterFirstStep(client *slack.Client, jobmanager manager.JobManager) *modals.FlowWithViewAndFollowUps {
-	return modals.ForView(Identifier, FirstStepView(client, jobmanager)).WithFollowUps(map[slack.InteractionType]interactions.Handler{
-		slack.InteractionTypeViewSubmission: processSubmissionHandler(client, jobmanager),
+	return modals.ForView(Identifier, FirstStepView()).WithFollowUps(map[slack.InteractionType]interactions.Handler{
+		slack.InteractionTypeViewSubmission: launchNextFirstStep(client, jobmanager),
 	})
 }
 
-// RegisterSecondStep creates a registration entry for the enhancement request form
+func launchNextFirstStep(updater modals.ViewUpdater, jobmanager manager.JobManager) interactions.Handler {
+	return interactions.HandlerFunc("launch2", func(callback *slack.InteractionCallback, logger *logrus.Entry) (output []byte, err error) {
+		go func() {
+			overwriteView := func(view slack.ModalViewRequest) {
+				// don't pass a hash so we overwrite the View always
+				response, err := updater.UpdateView(view, "", "", callback.View.ID)
+				if err != nil {
+					logger.WithError(err).Warn("Failed to update a modal View.")
+				}
+				logger.WithField("response", response).Trace("Got a modal response.")
+			}
+			overwriteView(SecondStepView(callback, jobmanager))
+		}()
+		response, err := json.Marshal(&slack.ViewSubmissionResponse{
+			ResponseAction: slack.RAUpdate,
+			View:           PrepareNextStepView(),
+		})
+		if err != nil {
+			logger.WithError(err).Error("Failed to marshal FirstStepView update submission response.")
+			return nil, err
+		}
+		return response, nil
+	})
+}
+
 func RegisterSecondStep(client *slack.Client, jobmanager manager.JobManager) *modals.FlowWithViewAndFollowUps {
-	return modals.ForView(Identifier2ndStep, FirstStepView(nil, nil)).WithFollowUps(map[slack.InteractionType]interactions.Handler{
-		slack.InteractionTypeViewSubmission: processSubmissionHandler2ndStep(client, jobmanager),
+	return modals.ForView(Identifier2ndStep, ThirdStepView(nil, jobmanager)).WithFollowUps(map[slack.InteractionType]interactions.Handler{
+		slack.InteractionTypeViewSubmission: launchNextSecondStep(client, jobmanager),
 	})
 }
 
-// RegisterThirdStep creates a registration entry for the enhancement request form
-func RegisterThirdStep(client *slack.Client, jobmanager manager.JobManager) *modals.FlowWithViewAndFollowUps {
-	return modals.ForView(Identifier3rdStep, FirstStepView(nil, nil)).WithFollowUps(map[slack.InteractionType]interactions.Handler{
-		slack.InteractionTypeViewSubmission: processSubmissionHandler3rdStep(client, jobmanager),
+func launchNextSecondStep(updater modals.ViewUpdater, jobmanager manager.JobManager) interactions.Handler {
+	return interactions.HandlerFunc("launch2", func(callback *slack.InteractionCallback, logger *logrus.Entry) (output []byte, err error) {
+		go func() {
+			overwriteView := func(view slack.ModalViewRequest) {
+				// don't pass a hash so we overwrite the View always
+				response, err := updater.UpdateView(view, "", "", callback.View.ID)
+				if err != nil {
+					logger.WithError(err).Warn("Failed to update a modal View.")
+				}
+				logger.WithField("response", response).Trace("Got a modal response.")
+			}
+			overwriteView(ThirdStepView(callback, jobmanager))
+		}()
+		response, err := json.Marshal(&slack.ViewSubmissionResponse{
+			ResponseAction: slack.RAUpdate,
+			View:           PrepareNextStepView(),
+		})
+		if err != nil {
+			logger.WithError(err).Error("Failed to marshal FirstStepView update submission response.")
+			return nil, err
+		}
+		return response, nil
 	})
 }
